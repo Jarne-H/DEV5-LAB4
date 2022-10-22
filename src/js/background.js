bb2fbc1d1aa0beae017eea412eb6854c44f8d3a2
Jarne_H <47690466+jarnejens@users.noreply.github.com>
2022-10-22 12:54:48 +0200

    build: set sphere radius to variable

--- src/js/background.js
+++ src/js/background.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
 
 //create function to create house
-export default function background() {
+export default function background(radius) {
     //create ball 50 wide
-    const geometryBall = new THREE.SphereGeometry(5, 32, 32);
+    const geometryBall = new THREE.SphereGeometry(radius/2, 32, 32);
     const materialBall = new THREE.MeshStandardMaterial({ color: 0x87ceff });
     const backgroundSphere = new THREE.Mesh(geometryBall, materialBall);
     //show inside
