5358d9a3c4004a08a111767d8d31d252cf86aa16
Jarne_H <47690466+jarnejens@users.noreply.github.com>
2022-10-24 11:37:26 +0200

    build: clean up code

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'THREE/examples/jsm/controls/OrbitControls.js';
 import {createHouse} from './src/js/house.js';
-import background from './src/js/background.js';
+import bGround from './src/js/bGround.js';
 import ground from './src/js/ground.js';
 import road from './src/js/road.js';
 import roadCube from './src/js/roadcube';
@@ -13,12 +13,17 @@ import { houseLight, directionalLight } from './src/js/lights.js';
 
 //prate ship downloaded at: https://sketchfab.com/3d-models/pirate-ship-6b32fb0dac4c4e79a2a09a93559302e8
 
-//*******YOU CAN CHANGE THESE VARIABLES******* */
+//******************************************** */
+//*****> YOU CAN CHANGE THESE VARIABLES <***** */
+//******************************************** */
 //size of house
 var houseWidth = 1;
 var houseDepth = 1;
+var houseHeight = 1;
 //size of world
 const radius = 10;
+//******************************************** */
+//******************************************** */
 
 
 //*******STARTUP******* */
@@ -55,29 +60,23 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-//*********************LIGHTS*********************//
+window.addEventListener('resize', onWindowResize, false);
+
+//*********************SPACE*********************//
 scene.add(houseLight())
 scene.add(directionalLight());
-
-//*********************BACKGROUND*********************//
-scene.add(background(radius));
-
-//*********************GROUND*********************//
+scene.add(bGround(radius));
 scene.add(ground(radius));
-
-//*********************ROAD*********************//
 scene.add(road(radius));
 scene.add(roadCube(radius, 0.5));
 scene.add(roadCube(radius, 1.5));
+for (let i = 0; i < radius*2; i++) {
+  scene.add(clouds(radius));
+}
 
 //*********************PRATESHIP*********************//
 
-
-//create new gltf loader
 const loader = new GLTFLoader();
-//load the gltf file
-
-window.addEventListener('resize', onWindowResize, false);
 loader.load('./src/models/scene.gltf', function (gltf) {
   var ship = gltf.scene;
   ship.scale.set(0.1, 0.1, 0.1);
@@ -89,20 +88,12 @@ loader.load('./src/models/scene.gltf', function (gltf) {
 });
 console.log(loader);
 
-//*********************CLOUDS*********************//
-for (let i = 0; i < radius*2; i++) {
-  scene.add(clouds(radius));
-}
-
 //*********************HOUSE*********************//
 
-//create a house with planes
-//create 6 flats for walls
+//create a house
 var houseX = 0;
 var houseY = 0;
 var houseZ = 0;
-var houseHeight = document.getElementById("houseHeight").value;
-
 
 //if no house is created, create house
 function House() {
@@ -118,23 +109,10 @@ function House() {
 }
 House();
 
-//*******PIRATESHIP ANIMATION******* */
-
-//move pirate ship to y = -5.5 then teleport back to 5.5 and repeat
-function moveShip() {
-  if (item.position.y > -5.5) {
-    item.position.y -= 0.01;
-  } else {
-    item.position.y = 5.5;
-  }
-}
-
-
 //*******ANIMATION******* */
 function animate() {
   requestAnimationFrame( animate );
 
   renderer.render(scene, camera);
-  //moveShip(pirateShip);
 };
 animate();
--- src/js/bGround.js
+++ src/js/bGround.js
@@ -1,12 +1,12 @@
 import * as THREE from 'three';
 
 //create function to create house
-export default function background(radius) {
+export default function bGround(radius) {
     //create ball 50 wide
     const geometryBall = new THREE.SphereGeometry(radius/2, 32, 32);
     const materialBall = new THREE.MeshStandardMaterial({ color: 0x87ceff });
-    const backgroundSphere = new THREE.Mesh(geometryBall, materialBall);
+    const bGroundSphere = new THREE.Mesh(geometryBall, materialBall);
     //show inside
-    backgroundSphere.material.side = THREE.BackSide;
-    return(backgroundSphere);
+    bGroundSphere.material.side = THREE.BackSide;
+    return(bGroundSphere);
 }
